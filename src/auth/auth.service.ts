a802fccf72ec1e532b76e4686b73631d460e3aef
cinsy26 <siusy2618@naver.com>
2026-01-01 23:31:08 +0900

    Feat: jwt 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { SignInLocalResponseDto } from './dto/signInlocal.response.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Sign } from 'crypto';
 
 //AuthService는 사용자를 검색하고 비밀번호를 검증하는 역할을 한다.
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   //회원가입 메서드
-  async signUp(email: string, password: string) {
+  async createUser(email: string, password: string) {
     const existUser = await this.usersService.findOneByEmail(email);
     if (existUser) {
       throw new Error('해당 이메일은 이미 회원가입이 되어있습니다.');
@@ -28,5 +33,12 @@ export class AuthService {
       return { email: user.email, id: user.id };
     }
     return null;
+  }
+
+  async login(user: SignInLocalResponseDto): Promise<{ access_token: string }> {
+    const payload = { userId: user.id, email: user.email };
+    return {
+      access_token: await this.jwtService.sign(payload),
+    };
   }
 }
